7ad9da97c7921362ab10fff72792066f644d0379
Wenna Han <wennahan98@gmail.com>
2024-07-09 12:06:22 -0400

    create server.r and ui.r files

--- server.R
+++ server.R
@@ -1,39 +1,65 @@
-library(shiny)
-library(dplyr)
-library(ggplot2)
-
-shinyServer(function(input, output, session) {
+server <- function(input, output, session) {
+  data <- reactiveVal()
   
-  getData <- reactive({
-    newData <- msleep %>% filter(vore == input$vore)
+  observeEvent(input$download_data, {
+    # For demonstration, we use static data instead of querying an API
+    bitcoin_data <- data.frame(
+      timestamp = as.POSIXct(c("2024-06-09 10:04:39", "2024-06-09 11:01:52", "2024-06-09 12:08:47", 
+                               "2024-06-09 13:05:10", "2024-06-09 14:02:01", "2024-06-09 15:05:09")),
+      price = c(69464.55, 69489.02, 69512.59, 69636.45, 69658.47, 69737.13),
+      volume = c(11297784379, 11553052271, 11572887602, 11652997674, 8892545791, 11181055188)
+    )
+    data(bitcoin_data)
   })
   
-  #create plot
-  output$sleepPlot <- renderPlot({
-    #get filtered data
-    newData <- getData()
-    
-    #create plot
-    g <- ggplot(newData, aes(x = bodywt, y = sleep_total))
-    
-    if(input$conservation){
-      g + geom_point(size = input$size, aes(col = conservation))
-    } else {
-      g + geom_point(size = input$size)
+  output$data_table <- renderDT({
+    req(data())
+    datatable(data())
+  })
+  
+  output$subset_ui <- renderUI({
+    req(data())
+    fluidRow(
+      column(6, checkboxGroupInput("subset_columns", "Columns to Display", choices = names(data()), selected = names(data()))),
+      column(6, sliderInput("subset_rows", "Rows to Display", min = 1, max = nrow(data()), value = c(1, nrow(data()))))
+    )
+  })
+  
+  output$download_csv <- downloadHandler(
+    filename = function() { "subset_data.csv" },
+    content = function(file) {
+      write.csv(data()[input$subset_rows[1]:input$subset_rows[2], input$subset_columns], file)
     }
-  })
+  )
   
-  #create text info
-  output$info <- renderText({
-    #get filtered data
-    newData <- getData()
-    
-    paste("The average body weight for order", input$vore, "is", round(mean(newData$bodywt, na.rm = TRUE), 2), "and the average total sleep time is", round(mean(newData$sleep_total, na.rm = TRUE), 2), sep = " ")
+  observeEvent(input$plot_data, {
+    req(data())
+    plot_data <- data()
+    if (input$plot_var == "Price") {
+      plot_data <- plot_data %>% select(timestamp, price)
+    } else {
+      plot_data <- plot_data %>% select(timestamp, volume)
+    }
+    output$data_plot <- renderPlot({
+      ggplot(plot_data, aes(x = timestamp, y = plot_data[[input$plot_var]])) +
+        geom_line(color = "blue", size = 1) +
+        geom_point(color = "red", size = 2) +
+        labs(title = paste("Bitcoin", input$plot_var, "Trend"),
+             x = "Timestamp",
+             y = input$plot_var) +
+        theme_minimal()
+    })
+    output$summary_output <- renderPrint({
+      summary_data <- plot_data[[input$plot_var]]
+      if (input$summary_type == "Mean") {
+        mean(summary_data)
+      } else if (input$summary_type == "Median") {
+        median(summary_data)
+      } else {
+        sum(summary_data)
+      }
+    })
   })
-  
-  #create output of observations    
-  output$table <- renderTable({
-    getData()
-  })
-  
-})
+}
+
+shinyApp(ui, server)
--- ui.R
+++ ui.R
@@ -1,26 +1,56 @@
+library(shiny)
+library(shinydashboard)
 library(ggplot2)
+library(dplyr)
+library(DT)
 
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Investigation of Mammal Sleep Data"),
-  
-  # Sidebar with options for the data set
-  sidebarLayout(
-    sidebarPanel(
-      h3("Select the mammal's biological order:"),
-      selectizeInput("vore", "Vore", selected = "omni", choices = levels(as.factor(msleep$vore))),
-      br(),
-      sliderInput("size", "Size of Points on Graph",
-                  min = 1, max = 10, value = 5, step = 1),
-      checkboxInput("conservation", h4("Color Code Conservation Status", style = "color:red;"))
-    ),
-    
-    # Show outputs
-    mainPanel(
-      plotOutput("sleepPlot"),
-      textOutput("info"),
-      tableOutput("table")
+ui <- dashboardPage(
+  dashboardHeader(title = "CoinGecko Cryptocurrencies Data Analysis"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("About", tabName = "about", icon = icon("info-circle")),
+      menuItem("Data Download", tabName = "data_download", icon = icon("download")),
+      menuItem("Data Exploration", tabName = "data_exploration", icon = icon("chart-line"))
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "about",
+              h2("About This App"),
+              p("This app provides analysis of cryptocurrencies data. The data includes current and historical market data for different cryptocurrencies/coins."),
+              p("The purpose of this app is to allow users to download, explore, and visualize cryptocurrencies data."),
+              p("Tabs:"),
+              p("1. About: Describes the app and data source."),
+              p("2. Data Download: Allows users to query, view, subset, and download the data."),
+              p("3. Data Exploration: Allows users to explore and visualize the data with various options.")
+      ),
+      tabItem(tabName = "data_download",
+              sidebarLayout(
+                sidebarPanel(
+                  dateInput("start_date", "Start Date", value = Sys.Date() - 30),
+                  dateInput("end_date", "End Date", value = Sys.Date()),
+                  actionButton("download_data", "Download Data")
+                ),
+                mainPanel(
+                  DTOutput("data_table"),
+                  uiOutput("subset_ui"),
+                  downloadButton("download_csv", "Download CSV")
+                )
+              )
+      ),
+      tabItem(tabName = "data_exploration",
+              sidebarLayout(
+                sidebarPanel(
+                  selectInput("plot_var", "Variable to Plot", choices = c("Price", "Volume")),
+                  selectInput("summary_type", "Summary Type", choices = c("Mean", "Median", "Sum")),
+                  actionButton("plot_data", "Generate Plot")
+                ),
+                mainPanel(
+                  plotOutput("data_plot"),
+                  verbatimTextOutput("summary_output")
+                )
+              )
+      )
     )
   )
-))
+)
